2f9b5a63121a8b509652e3bf044e39463c11838c
JL-Huawei_Graznov <graznov@me.com>
2025-03-19 21:31:17 +0300

    19.03.25 creat

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const { connectToDb, getDb } = require("./db");
 const PORT = 3000;
 
 const app = express();
+// app.use(cors(corsOptions));
+app.use(express.json());
+// app.use(cookieParser());
 
 let db;
 
@@ -18,3 +21,26 @@ connectToDb((err) => {
     console.log(`DB connection error: ${err}`);
   }
 });
+
+const handleError = (res, error) => {
+  res.status(500).json({ error })
+}
+
+app.get("/pdd/", (req, res) => {
+
+  let arr = []
+
+  db
+      .collection("pddcollection")
+      .find()
+      .forEach(e=>arr.push(e))
+      .then((doc)=>{
+          res
+              .status(200)
+              .json(arr)
+        })
+      .catch(()=> handleError(res, 'Something went wrong.'))
+
+  console.log(arr)
+
+})
